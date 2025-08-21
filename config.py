00fb3b24e232a5325cb09a4efb5caa40691a469b
Imam Sya'bana <imambritish@gmail.com>
2025-08-21 17:50:17 +0700

    vercel

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
-    SQLALCHEMY_DATABASE_URI= os.getenv('Database_URL', 'postgresql://neondb_owner:npg_0nqtbHFOEG7P@ep-morning-queen-adhe20kk-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
