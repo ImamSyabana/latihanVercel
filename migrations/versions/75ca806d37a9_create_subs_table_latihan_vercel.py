635c7e891c393b16cbb088932cca3f371a476694
Imam Sya'bana <imambritish@gmail.com>
2025-08-21 23:32:59 +0700

    adding migration

--- migrations/versions/75ca806d37a9_create_subs_table_latihan_vercel.py
+++ migrations/versions/75ca806d37a9_create_subs_table_latihan_vercel.py
@@ -1,8 +1,8 @@
-"""create subs table latihan
+"""Create subs table latihan vercel
 
-Revision ID: 312649e2eea1
+Revision ID: 75ca806d37a9
 Revises: 
-Create Date: 2025-08-21 21:27:06.623286
+Create Date: 2025-08-21 23:31:58.708159
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '312649e2eea1'
+revision = '75ca806d37a9'
 down_revision = None
 branch_labels = None
 depends_on = None
