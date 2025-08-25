c5b95ad1cf276012eeb6113ac28e0e75fffc0447
Imam Sya'bana <imambritish@gmail.com>
2025-08-25 10:31:10 +0700

    lanjutin bikin route login register logout dll

--- app/__init__.py
+++ app/__init__.py
@@ -4,45 +4,46 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import logging
 from flask_ckeditor import CKEditor
-
 from flask_bootstrap import Bootstrap5
 from flask_login import LoginManager
 
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-app.config.from_object(Config)
-ckeditor = CKEditor(app)
-Bootstrap5(app)
-
-
-# TODO: Configure Flask-Login
-# initiate login manager object
+db = SQLAlchemy()
+migrate = Migrate()
+ckeditor = CKEditor()
+bootstrap = Bootstrap5()
 login_manager = LoginManager()
-# conect login manager dengan app
-login_manager.init_app(app)
 
-#Set the login view: Flask-Login will redirect unauthenticated users to this route.
-login_manager.login_view = "login"
+def create_app():
+    app = Flask(__name__)
 
+    from app.routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+    app.config.from_object(Config)
+    ckeditor.init_app(app)
+    bootstrap.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login_manager.init_app(app)
+    login_manager.login_view = "login"
+
+    from app import routes, models
+
+    # Setup console logging
+    if not app.debug:
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        app.logger.addHandler(stream_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Flask App startup')
+
+    return app
 
 @login_manager.user_loader
 def load_user(id):
     if id:
-        return db.get_or_404(models.User, int(id))
+        from app import models
+        return db.session.get(models.User, int(id))
     return None
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-from app import routes, models
-
-# Setup console logging
-if not app.debug:
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.INFO)
-    app.logger.addHandler(stream_handler)
-
-app.logger.setLevel(logging.INFO)
-app.logger.info('Flask App startup')
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,11 @@
-from flask import render_template, request, redirect, url_for, flash
-from app import app, db
+from flask import render_template, request, redirect, url_for, flash, Blueprint
+from app import db
 from app.models import BlogPost, User
 
-@app.route('/')
+main = Blueprint('main', __name__)
+
+
+@main.route('/')
 def get_all_posts():
     # data yang ada di tabel blog_posts
     result = db.session.execute(db.select(BlogPost))
@@ -10,7 +13,7 @@ def get_all_posts():
 
     # data yang ada di tabel user
     
-    user_results = db.session.execute(db.Select(User))
+    user_results = db.session.execute(db.select(User))
     users = user_results.scalars().all()
 
     return render_template("index.html", all_posts=posts, users = users)
