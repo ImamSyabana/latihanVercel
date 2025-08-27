8f7462a21628d36deb862f3c5a1604223e656027
Imam Sya'bana <imambritish@gmail.com>
2025-08-27 22:07:09 +0700

    vercel deploy problem

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24))
+    SECRET_KEY = os.getenv('SECRET_KEY', '8BYkEfBA6O6donzWlSihBXox7C0sKR6b')
     SQLALCHEMY_DATABASE_URI= os.getenv('Database_URL', 'postgresql://neondb_owner:npg_yq8e9NTzQvjr@ep-shy-wildflower-adhv56i1-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
