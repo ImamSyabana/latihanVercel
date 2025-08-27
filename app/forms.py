4eca3030efeff5362741abdd063e704fa8177f00
Imam Sya'bana <imambritish@gmail.com>
2025-08-27 16:19:18 +0700

    latihan vercel

---
+++ app/forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap5
+from flask import Flask, render_template, redirect, url_for
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+# WTForm for creating a blog post
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
+
+# TODO: Create a RegisterForm to register new users
+class RegisterForm(FlaskForm):
+    email = StringField(label = "Email", validators = [DataRequired()])
+    name = StringField(label = "Name", validators = [DataRequired()])
+    password = PasswordField(label = "Password", validators = [DataRequired()])
+    submit = SubmitField('SIGN ME UP!')
+
+# TODO: Create a LoginForm to login existing users
+#Create a LoginForm to login existed users
+class LoginForm(FlaskForm):
+    email = StringField(label = "Email", validators = [DataRequired()])
+    password = PasswordField(label = "Password", validators = [DataRequired()])
+    submit = SubmitField('LET ME IN!')
+
+# TODO: Create a CommentForm so users can leave comments below posts
+class CommentForm(FlaskForm):
+    text = CKEditorField(label = 'Comment', validators=[DataRequired()])
+    submit = SubmitField('SUBMIT COMMENT')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,10 @@
 from flask import render_template, request, redirect, url_for, flash, Blueprint
 from app import db
 from app.models import BlogPost, User
+from app.forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user,login_required
 
 main = Blueprint('main', __name__)
 
@@ -19,3 +23,90 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts, users = users)
 
 
+# TODO: Retrieve a user from the database based on their email. 
+@main.route('/login', methods = ["GET", "POST"])
+def login():
+    form = LoginForm()
+
+    if request.method == "POST":
+        # mengambil yang ada di kolom input HTML
+        email = form.email.data
+        password = form.password.data
+
+        # mengambil data user yang ada di database
+        user_to_login = db.session.execute(db.Select(User).where(User.email == email)).scalar()
+
+        # Login and validate the user.
+        # user should be an instance of your `User` class
+        # Check if the user exists and the password is correct
+        if not(user_to_login):
+            # Handle invalid credentials
+            flash('That email does not exist. Please try again.', 'error')
+            return redirect(url_for('login'))
+        elif check_password_hash(user_to_login.password, password) == False:
+            # Handle invalid credentials
+            flash('Password Incorrect. Please try again.', 'error')
+            return redirect(url_for('login'))
+        else:
+            #flash('You were successfully logged in!', 'success')
+            login_user(user_to_login)
+            return redirect(url_for("get_all_posts"))
+        
+    return render_template("login.html", form = form)
+
+
+# TODO: Use Werkzeug to hash the user's password when creating a new user.
+@main.route('/register', methods = ["GET", "POST"])
+def register():
+    form = RegisterForm()
+
+    if request.method == "POST":
+        # mengambil yang ada di kolom input HTML
+        name = form.name.data
+        email = form.email.data
+
+        # convert password menjadi hash + salt
+        password = form.password.data
+        password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+        
+        user_to_register = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        
+        # kalo email sudah exist maka user_to_register ada isinya 
+        # berarti ngga boleh register
+        if user_to_register:
+            # Handle existed credentials
+            flash('You have signed up with that email, log in instead.', 'error')
+            return redirect(url_for('login'))
+        
+        # kalo email belum exist berarti user_to_register return None
+        # berarti email belum pernah dipake dan boleh register.
+        elif not(user_to_register):
+            # menyiapkan records object USER untuk dimasukkan ke database
+            new_user = User(
+                email = email,
+                password = password,
+                name = name
+            )
+
+            # menambahkan data ke datatbase.
+            db.session.add(new_user)
+            db.session.commit()
+            
+            # login the new user
+            login_user(new_user)
+
+            return redirect(url_for('get_all_posts'))
+    return render_template("register.html", form = form)
+
+@main.route("/about")
+def about():
+    return render_template("about.html")
+
+@main.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('get_all_posts'))
+
+@main.route("/contact")
+def contact():
+    return render_template("contact.html")
